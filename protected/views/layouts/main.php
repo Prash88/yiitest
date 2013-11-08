44d265ee5bd047203e0b445eb0c9e44ff837a7d0
Prash88 <prash88@iastate.edu>
2013-11-08 12:26:15 -0600

    Adding Ui

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -4,19 +4,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="en" />
-
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
-
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-	
 	<?php Yii::app()->bootstrap->register(); ?>
-	
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 </head>
 
@@ -33,7 +21,9 @@
     </div>
 	<?php echo $content; ?>
 	<div id="footer">
+	<div class="container">
         <p class="muted credit">&copy; 2013 Prashanth</p>
+      </div>
     </div>
 </body>
 </html>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,5 +1,38 @@
 <?php
 /* @var $this SiteController */
-
 $this->pageTitle=Yii::app()->name;
 ?>
+<div class="container">
+
+	<div class="row">
+		<div class="span12">
+			<div  class="span4">
+				<textarea rows="20" style="resize: none; " disabled></textarea>
+			</div>
+			<div  class="span6">
+				<textarea rows="20" style="resize: none; " placeholder="/*Java code here*/"></textarea>
+			</div>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="span12">
+			<div  class="span4">
+				<textarea rows="3" style="resize: none; " disabled></textarea>
+			</div>
+			<div  class="span6">
+				<textarea rows="3" style="resize: none; " disabled></textarea>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+			<div class="span12">
+	
+		<div class="span3">
+
+	<?php echo TbHtml::button('Run',
+    array('color' => TbHtml::BUTTON_COLOR_PRIMARY, 'size' => TbHtml::BUTTON_SIZE_SMALL)); ?>		</div>
+	
+	</div>
+	</div>
+</div>
